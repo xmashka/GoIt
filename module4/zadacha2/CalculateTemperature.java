27d8a2c58882e46c55098511b3e7339d9e0560d5
Vlasenko Mariya H <xmashka@gmail.com>
2016-03-09 22:49:41 +0200

    change module 4 zadacha 2

--- module4/zadacha2/CalculateTemperature.java
+++ module4/zadacha2/CalculateTemperature.java
@@ -7,17 +7,14 @@ public class CalculateTemperature {
     public static void main(String[] args) {
         System.out.println("Choose original measuring 1-Celsius, 2-Fahrenheit ");
         Scanner tm = new Scanner(System.in);
+        GetOriginData data = new GetOriginData();
         switch (tm.nextInt()) {
             case 1:
-                GetOriginData DataCelsius = new GetOriginData();
-                Celsius2Fahrenheit MeasurCelsius = new Celsius2Fahrenheit(GetOriginData.GetData("Celsius"));
+                Celsius2Fahrenheit MeasurCelsius = new Celsius2Fahrenheit(data.GetData("Celsius"));
                 break;
             case 2:
-               GetOriginData DataFahrenheit = new GetOriginData();
-                Fahrenheit2Celsius MeasurFahrenheit = new Fahrenheit2Celsius(GetOriginData.GetData("Fahrenheit"));
+                Fahrenheit2Celsius MeasurFahrenheit = new Fahrenheit2Celsius(data.GetData("Fahrenheit"));
                 break;
         }
-
-
     }
 }
