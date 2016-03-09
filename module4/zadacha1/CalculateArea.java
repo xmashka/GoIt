a41b7f196461f75abbe0562fa0eb2d880f435f4c
Vlasenko Mariya W <xmashka@gmail.com>
2016-03-09 16:15:34 +0200

    change module 4 zadacha 1

--- module4/zadacha1/CalculateArea.java
+++ module4/zadacha1/CalculateArea.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class CalculateArea {
     public static void main(String[] args){
         System.out.println("Input type of figure 1-triangle 2-rectangle 3-circle ");
-        Scanner tf = new Scanner(System.in);
-        GetDataFigure newFigure = new GetDataFigure(tf.nextInt());
+        Scanner typeFigure = new Scanner(System.in);
+        GetDataFigure newFigure = new GetDataFigure(typeFigure.nextInt());
     }
 }
--- module4/zadacha1/CalculateDataFigure.java
+++ module4/zadacha1/CalculateDataFigure.java
@@ -3,26 +3,20 @@ package module4.zadacha1;
 public class CalculateDataFigure {
     static final float PI = 3.14F;
 
-    CalculateDataFigure(int param1, int param2, int param3){
-        float area = param2 * param3;
-        if (1 == param1) {
-            area = area /2;
+    CalculateDataFigure(int typeFigure, int lehght1, int lehght2){
+        float area;
+        if (typeFigure == 1) {
+            area = lehght1 * lehght2 / 2;
         }
-//        area = 1 == param1 ? area/2 : area;
-//        if (param1 == 1) {
-//            area = param2 * param3 / 2;
-////            System.out.println("Area is " + area);
-//        }
-//        else if (param1 == 2) {
-//
-////            System.out.println("Area is " + area);
-//        }
+        else {
+            area = lehght1 * lehght2;
+        }
         System.out.println("Area is " + area);
     }
 
-    public CalculateDataFigure(int param4) {
+    public CalculateDataFigure(int radiusCircle) {
         double area;
-        area = PI*Math.pow(param4,2);
+        area = PI*Math.pow(radiusCircle,2);
         System.out.println("Area is " + area);
     }
 }
