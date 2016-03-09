d6c32af29b0a5203300b88446ee79e148d2f3272
Vlasenko Mariya H <xmashka@gmail.com>
2016-03-09 22:56:06 +0200

    change module 4 zadacha 2

--- module4/zadacha2/GetOriginData.java
+++ module4/zadacha2/GetOriginData.java
@@ -3,7 +3,7 @@ package module4.zadacha2;
 import java.util.Scanner;
 
 public class GetOriginData {
-    public static int GetData (String typeMeasure){
+    public int GetData (String typeMeasure){
         System.out.println("Input " + typeMeasure + " temperature  ");
         Scanner od = new Scanner(System.in);
         return od.nextInt();
